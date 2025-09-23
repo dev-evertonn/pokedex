80dfc8a118b6f9d78e1ddeb45116783d96a19dc7
dev-evertonn <evertonporalves73@gmail.com>
2025-09-22 22:35:44 -0300

    update paginacao

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,39 @@
-function convertPokemonToList(pokemon) {
-    return `
-        <li class="pokemon" class="${pokemon.type}">
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5;
+let offset = 0
+const maxRecords = 15;
+
+function loadPokemonList(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+        <li class="pokemon ${pokemon.type}">
             <span class="number">#0${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    ${pokemon.types.map((type) => `<li>${type}</li>`).join('')}
+                    ${pokemon.types.map((type) => `<li class="${type}">${type}</li>`).join('')}
                 </ol>
                 <img src="${pokemon.img}" alt="${pokemon.name}">
             </div>
         </li>
-    `
+    `).join('')
+        pokemonList.innerHTML += newHtml;
+    })
 }
 
-const pokemonList = document.getElementById('pokemonList');
+loadPokemonList(offset, limit);
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML = pokemons.map(convertPokemonToList).join('')
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNextPage = offset + limit;
+
+    if (qtdRecordsWithNextPage >= maxRecords) {
+        const newlimit = maxRecords - offset;
+        loadPokemonList(offset, newlimit);
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
+    } else {
+        loadPokemonList(offset, limit);
+    }
 })
